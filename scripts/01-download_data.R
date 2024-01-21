a940aa546891549571399569b0b8e4ba3c2f24eb
Bellamaclean7 <bellamaclean7@gmail.com>
2024-01-21 18:00:03 +0000

    Download raw data and saved

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -17,6 +17,10 @@ library(opendatatoronto)
 library(dplyr)
 library(readr) 
 
+install.packages("opendatatoronto")
+library(opendatatoronto)
+library(dplyr)
+
 # get package
 package <- show_package("neighbourhood-crime-rates")
 package
@@ -31,9 +35,5 @@ datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'
 data <- filter(datastore_resources, row_number()==1) %>% get_resource()
 data
 
-#### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
-
+write_csv(data, "inputs/data/raw_data.csv")
          
